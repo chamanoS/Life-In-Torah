cfc866db960e721fb1f87cd7d477694f315e07ba
Chamano Ramashia <chamano.sr@gmail.com>
2025-09-13 21:54:44 +0200

    adding form for adding a book manualy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,15 +4,71 @@ import { Link } from "react-router-dom";
 
 function Home() {
   const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState({
+    title: "",
+    author: "",
+    description: "",
+    cover: ""
+  });
 
   useEffect(() => {
     axios.get("http://localhost:5000/books")
       .then(res => setBooks(res.data));
   }, []);
 
+  const addBook = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/books", newBook)
+      .then(res => {
+        setBooks([...books, res.data]);
+        setNewBook({ title: "", author: "", description: "", cover: "" });
+      });
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Book Library</h1>
+
+      {/* Add Book Form */}
+      <form onSubmit={addBook} style={{
+        marginBottom: "30px",
+        padding: "15px",
+        border: "1px solid #ddd",
+        borderRadius: "8px",
+        maxWidth: "500px"
+      }}>
+        <h3>Add a New Book</h3>
+        <input
+          type="text"
+          placeholder="Title"
+          value={newBook.title}
+          onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+          style={{ width: "100%", margin: "5px 0", padding: "8px" }}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={newBook.author}
+          onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+          style={{ width: "100%", margin: "5px 0", padding: "8px" }}
+        />
+        <textarea
+          placeholder="Description"
+          value={newBook.description}
+          onChange={(e) => setNewBook({ ...newBook, description: e.target.value })}
+          style={{ width: "100%", margin: "5px 0", padding: "8px" }}
+        />
+        <input
+          type="text"
+          placeholder="Cover Image URL"
+          value={newBook.cover}
+          onChange={(e) => setNewBook({ ...newBook, cover: e.target.value })}
+          style={{ width: "100%", margin: "5px 0", padding: "8px" }}
+        />
+        <button type="submit" style={{ marginTop: "10px" }}>Add Book</button>
+      </form>
+
+      {/* Book Grid */}
       <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", gap: "20px" }}>
         {books.map(b => (
           <div key={b.id} style={{
